40e347ed47cfca1173657ad82976ad4a78468f32
theshark-fb <theshark-fb@hotmail.fr>
2013-02-25 16:54:22 +0100

    Mise à jour déplacement pion

--- chessFT/src/Fenetre.java
+++ chessFT/src/Fenetre.java
@@ -21,16 +21,16 @@ public class Fenetre extends JFrame {
 		Fenetre fenetre = new Fenetre();
 		Plateau plat = new Plateau();
 		
-		Case[] cases = Plateau.getCase(2, 5).getPiece().destinations();
+		//Case[] cases = Plateau.getCase(2, 5).getPiece().destinations();
 		Plateau.getCase(2, 5).setSurlignee("selection");
-		System.out.println(cases.length);
+		//System.out.println(cases.length);
 		/*for(int i=0;i<cases.length;i++){
 			int pos_x = cases[i].getPositionX();
 			int pos_y = cases[i].getPositionY();
 			Plateau.getCase(pos_x, pos_y).setSurlignee("destination");
 			System.out.println(i);
 		}*/
-		Plateau.getCase(2, 6).setSurlignee("destination");
+		//Plateau.getCase(2, 6).setSurlignee("selection");
 		
 		plat.repaint();
 
--- chessFT/src/Pion.java
+++ chessFT/src/Pion.java
@@ -30,27 +30,30 @@ public class Pion extends Piece {
 		int pos_x = this.m_positionX;
 		int pos_y = this.m_positionY;
 		if(this.m_couleur == "blanche")
-			pos_y += 1;
+			pos_x += 1;
 		else
-			pos_y -= 1;
-		if(pos_y>0 && pos_y<7){
+			pos_x -= 1;
+		if(pos_x>0 && pos_x<7){
+			//ok
 			if(Plateau.getCase(pos_x, pos_y).getPiece() == null){
 				cases[i] = Plateau.getCase(pos_x, pos_y);
 				i++;
 			}
-			if(pos_x>0){
-				if(Plateau.getCase(pos_x-1, pos_y).getPiece() != null && Plateau.getCase(pos_x-1, pos_y).getPiece().getCouleur() != this.getCouleur()){
-					cases[i] = Plateau.getCase(pos_x-1, pos_y);
+			if(pos_y>0){
+				//ok
+				if(Plateau.getCase(pos_x, pos_y-1).getPiece() != null && Plateau.getCase(pos_x, pos_y-1).getPiece().getCouleur() != this.getCouleur()){
+					cases[i] = Plateau.getCase(pos_x, pos_y-1);
 					i++;
 				}
 			}
-			if(pos_x<7){
-				if(Plateau.getCase(pos_x+1, pos_y).getPiece() != null && Plateau.getCase(pos_x, pos_y).getPiece().getCouleur() != this.getCouleur()){
-					cases[i] = Plateau.getCase(pos_x+1, pos_y);
+			if(pos_y<7){
+				//ok
+				if(Plateau.getCase(pos_x, pos_y+1).getPiece() != null && Plateau.getCase(pos_x, pos_y+1).getPiece().getCouleur() != this.getCouleur()){
+					cases[i] = Plateau.getCase(pos_x, pos_y+1);
 					i++;
 				}
 			}
-			if(m_premierDeplacement && Plateau.getCase(pos_x, pos_y) == null){
+			if(m_premierDeplacement && Plateau.getCase(pos_x, pos_y) == null && Plateau.getCase(pos_x+pos_x, pos_y) == null){
 				cases[i] = Plateau.getCase(pos_x, pos_y+pos_y);
 				i++;
 			}
